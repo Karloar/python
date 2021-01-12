d505646f9f055c40cea0c1db5feb48a150062fbf
Karloar <879865529@qq.com>
2021-01-12 20:41:34 +0800

    add a new branch for framework

--- multi_task_framework/__init__.py
+++ multi_task_framework/__init__.py
@@ -5,43 +5,54 @@ from concurrent.futures import ProcessPoolExecutor
 
 class Framework:
 
-    def __init__(self, start_generator, max_processes=10, max_threads=100):
-        self.start_generator = start_generator() if callable(start_generator) else start_generator
-        self.max_processes = max_processes
-        self.max_threads = max_threads
-        self.tasks = []
-        self.results = []
-        self.temp_results = []
+    def __init__(self, start_values, MAX_THREADS=100, MAX_PROCESSORS=10):
+        self.start_values = start_values
+        self.MAX_THREADS = MAX_THREADS
+        self.MAX_PROCESSORS = MAX_PROCESSORS
+        self.base_task = None
+        self.base_task_tmp = None
+        self.sub_task = None
+        self.final_results = []
 
-    def add_task(self, task_func, *args, **kwargs):
-        if len(self.tasks) > 1:
-            raise Exception("There can't be more than 2 tasks!")
-        self.tasks.append(partial(task_func, *args, **kwargs))
+    def add_base_task(self, base_task, *args, **kwargs):
+        if self.sub_task:
+            raise Exception("Base task must not run after sub task!")
+        self.base_task = partial(base_task, *args, **kwargs)
+        self.base_task_tmp = self.base_task
 
-    def run_thread_task(self, task, ctrl_generator):
-        task_pool = ThreadPoolExecutor(max_workers=self.max_threads)
+    def add_sub_task(self, sub_task, *args, **kwargs):
+        if not self.base_task:
+            raise Exception("There is no base task!")
+        self.sub_task = partial(sub_task, *args, **kwargs)
+        self.base_task = self.new_base_task
+
+    def new_base_task(self, base_task_key):
+        params = self.base_task_tmp(base_task_key)
+        return self.run_thread_task(self.sub_task, params, base_task_key)
+
+    def run_thread_task(self, task, params, base_task_key):
+        task_pool = ThreadPoolExecutor(max_workers=self.MAX_THREADS)
         task_results = []
-        for elem in ctrl_generator:
-            task_pool.submit(task, elem).add_done_callback(lambda future: task_results.append(future.result()))
+        if self.sub_task:
+            for elem in params:
+                task_pool.submit(task, elem, base_task_key).add_done_callback(lambda future: task_results.append(future.result()))
+        else:
+            for elem in params:
+                task_pool.submit(task, elem).add_done_callback(lambda future: task_results.append(future.result()))
         task_pool.shutdown()
         return task_results
 
-    def run_process_task(self, task, ctrl_generator, callback):
-        task_pool = ProcessPoolExecutor(max_workers=self.max_processes)
+    def run_process_task(self, task, params):
+        task_pool = ProcessPoolExecutor(max_workers=self.MAX_PROCESSORS)
         task_results = []
-        for elem in ctrl_generator:
-            task_pool.submit(task, elem).add_done_callback(lambda future: task_results.extend(list(callback(future.result()))))
+        for elem in params:
+            task_pool.submit(task, elem).add_done_callback(lambda future: task_results.extend(list(future.result())))
         task_pool.shutdown()
         return task_results
 
     def run(self):
-        if len(self.tasks) == 0:
-            raise Exception('There is no task!')
-        if len(self.tasks) == 1:
-            self.results.extend(self.run_thread_task(self.tasks[0], self.start_generator))
-        elif len(self.tasks) == 2:
-            self.results.extend(self.run_process_task(
-                self.tasks[0], self.start_generator,
-                partial(self.run_thread_task, self.tasks[1])
-            ))
-        return self.results
+        if not self.base_task:
+            raise Exception("There is no base task!")
+        if not self.sub_task:
+            return self.run_thread_task(self.base_task, self.start_values, None)
+        return self.run_process_task(self.base_task, self.start_values)
--- multi_task_framework/main.py
+++ multi_task_framework/main.py
@@ -11,28 +11,28 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def start_generator():
+def start_values():
     return range(10)
 
 
 def task1(a):
     time.sleep(1)
-    print(f'task1 --- {a}\n')
-    return list(map(lambda x: x * a, range(3)))
+    print(f'start task {a} ....\n')
+    return list(map(lambda x: x * a, range(3000)))
 
 
-def task2(b):
+def task2(b, a):
     time.sleep(1)
-    print(f'task2 --- {b}\n')
+    print(f'processing task {a} thread {b} ...\n')
     return b + 100
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
-    framework = Framework(start_generator())
-    framework.add_task(task1)
-    framework.add_task(task2)
+    framework = Framework(start_values())
+    framework.add_base_task(task1)
+    framework.add_sub_task(task2)
     result = framework.run()
     print(result)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
